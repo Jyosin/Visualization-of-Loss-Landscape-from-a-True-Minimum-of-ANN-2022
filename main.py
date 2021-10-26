20b246771e6650e04d315774566e01effea456ee
Wang Ruqin <miaomiao3141@yahoo.com>
2021-10-26 16:00:09 +0900

    feat:spped up.

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
                     'optimizer':{'name':'SGD','learning_rate':0.001},
                     'dataset':{'name':'uniform','batch_size':100,'epoch':1},
                     'model':{'name':'DNN','units':[64,16,1],
-                             'activations':['tanh','tanh','tanh']},'fuse_models':None }
+                             'activations':['tanh','tanh','tanh'],'fuse_models':None} }
     trainer = Trainer(trainer_args)
     trainer.just_build()
     trainer.model.summary()
