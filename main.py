b9802f941725430e32ddf75db26ae53da8b66af9
Wang Ruqin <miaomiao3141@yahoo.com>
2021-11-22 01:56:12 +0900

    uniform_trainer

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
 from utils import *
 import argparse
 from trainer.uniform_trainer import UniformTrainer
-from trainer.cifa10_trainer import Cifar10Trainer
+from trainer.cifar10_trainer import Cifar10Trainer
 from plotter import Plotter
 import os
-
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-    
+import pdb
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='Plot lossland around ture minima.')
-    parser.add_argument('--config', default='scripts/configs/1d_uniform_dnn.yaml')
+    parser.add_argument(
+        '--config', default='scripts/configs/1d_uniform_dnn.yaml')
+
     args = parser.parse_args()
     config = get_yml_content(args.config)
 
     trainer_args = config['Trainer']
     plotter_args = config['Plotter']
-
+    pdb.set_trace()
     if trainer_args['dataset']['name'] == 'uniform':
         trainer = UniformTrainer(trainer_args)
     elif trainer_args['dataset']['name'] == 'cifar10':
