36ba24943385238b7c83506d5bdb0682ddd08fd2
Wang Ruqin <miaomiao3141@yahoo.com>
2021-11-30 23:38:48 +0900

    test.

--- utils.py
+++ utils.py
@@ -4,22 +4,26 @@ from colorama import Fore
 
 ERROR_INFO = 'ERROR: '
 NORMAL_INFO = 'INFO: '
-WARNING_INFO = 'WARNING'
+WARNING_INFO = 'WARNING: '
+
 
 def write_to_file(path, filename, s):
     check_mkdir(path=path)
-    file = os .path.join(path, filename)
-    with open(file=file,mode='a')as f:
+    file = os.path.join(path, filename)
+    with open(file=file, mode='a') as f:
         f.writelines(s+'\n')
 
+
 def check_file(file):
-    if not os.path.exists(path = file):
-        print_error("no such file:{}. format(file)")
+    if not os.path.exists(path=file):
+        print_error("no such file: {}".format(file))
+
 
 def check_mkdir(path):
     if not os.path.exists(path=path):
-        print("no such path: {}, but we made.".format(path))
+        print_warning("no such path: {}, but we made.".format(path))
         os.makedirs(path)
+
 
 def get_yml_content(file_path):
     '''Load yaml file content'''
@@ -34,31 +38,42 @@ def get_yml_content(file_path):
         print_error(exception)
         exit(1)
 
+
+def print_error(*content):
+    '''Print error information to screen'''
+    print(Fore.RED + ERROR_INFO + ' '.join([str(c)
+                                            for c in content]) + Fore.RESET)
+
+
 def print_green(*content):
     '''Print information to screen in green'''
-    print(Fore.RED + ERROR_INFO + ''.join([str(c)
-                                            for c in content])+ Fore.RESET)
+    print(Fore.GREEN + ' '.join([str(c) for c in content]) + Fore.RESET)
+
 
 def print_normal(*content):
     '''Print error information to screen'''
     print(NORMAL_INFO, *content)
+
 
 def print_warning(*content):
     '''Print warning information to screen'''
     print(Fore.YELLOW + WARNING_INFO + ' '.join([str(c) for c in content]) +
           Fore.RESET)
 
+
 def print_dict(d, indent=0):
-    for key,value in d.items():
+    for key, value in d.items():
         if isinstance(value, dict):
             print_green('\t' * (indent) + key + ": ")
             print_dict(value, indent + 1)
         else:
             try:
-                print('\t' * (indent) + str(key) + ": "+ str(value))
+                print('\t' * (indent) + str(key) + ": " + str(value))
             except:
-                print_error("value can not print : ", type(value))
+                print_error("value can not print: ", type(value))
                 raise
-if __name__ == 'main':
-    print_error("zhang en zhi")
-    print("zhang en zhi")
+
+
+if __name__ == '__main__':
+    print_error("wang ru qin")
+    print("wang ru qin")
